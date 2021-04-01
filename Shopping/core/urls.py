63b771b44f76d9e82201313837c47e07a90444d8
fahiz <fahizkpzewia@gmail.com>
2021-04-01 11:48:08 +0530

    Order-summary Completed 01/04/21

--- Shopping/core/urls.py
+++ Shopping/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from .views import  HomeView,checkout,ItemDetailView,add_to_cart,remove_from_cart,OrderSummeryView
+from .views import  HomeView,checkout,ItemDetailView,add_to_cart,remove_from_cart,OrderSummeryView,remove_single_item_from_cart
 
 app_name ='core'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('checkout/',checkout,name='checkout'),
      path('product/<slug>/',ItemDetailView.as_view(),name='product'),
      path('add-to-cart/<slug>/',add_to_cart,name='add-to-cart'),
-     path('remove-from-cart/<slug>/',remove_from_cart,name='remove-from-cart')
+     path('remove-from-cart/<slug>/',remove_from_cart,name='remove-from-cart'),
+     path('remove_single_item_from_cart/<slug>/',remove_single_item_from_cart,name='remove_single_item_from_cart')
 ]
--- Shopping/core/views.py
+++ Shopping/core/views.py
@@ -13,7 +13,6 @@ class HomeView(ListView):
     paginate_by = 12
     template_name = "home_list.html"
 
-
 class OrderSummeryView(LoginRequiredMixin,View):
     def get(self, *args, **kwargs):
         try:
@@ -25,7 +24,6 @@ class OrderSummeryView(LoginRequiredMixin,View):
         except ObjectDoesNotExist:
             messages.error(request, "You do not have active order ")
             return redirect("/")
-
 
 class ItemDetailView(DetailView):
     model = Item
@@ -44,16 +42,18 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated. ")
+            return redirect("core:order-summery")
         else:
             messages.info(request, "This item was added to your cart. ")
             order.items.add(order_item)
+            return redirect("core:order-summery")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart. ")
-    return redirect("core:product", slug=slug)
+    return redirect("core:order-summery")
 
 @login_required
 def remove_from_cart(request, slug):
@@ -67,7 +67,31 @@ def remove_from_cart(request, slug):
                 item=item, user=request.user, ordered=False)[0]
             order.items.remove(order_item)
             messages.info(request, "This item was removed to your cart. ")
-            return redirect("core:product", slug=slug)
+            return redirect("core:order-summery")
+        else:
+            messages.info(request, "This item was not in your cart. ")
+            return redirect("core:order-summery")
+    else:
+        messages.info(request, "You do not have an active order.")
+        return redirect("core:order-summery")
+        
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item, user=request.user, ordered=False)[0]
+            if order_item.quantity >1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantuty was Updated. ")
+            return redirect("core:order-summery")
         else:
             messages.info(request, "This item was not in your cart. ")
             return redirect("core:product", slug=slug)
@@ -78,7 +102,6 @@ def remove_from_cart(request, slug):
 
 def checkout(request):
     return render(request, 'checkout-page.html')
-
 
 def product(request):
     return render(request, 'product-page.html')
